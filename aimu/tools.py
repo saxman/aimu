f96112e183488e3e4ab8a2d820af7458ad7f4820
Paul R Saxman <saxman@gmail.com>
2025-06-20 10:01:57 -0700

    feat: enhance MCPClient initialization and add tests for various configurations

--- aimu/tools.py
+++ aimu/tools.py
@@ -1,24 +1,58 @@
+from fastmcp import FastMCP
 from fastmcp import Client
 
 import asyncio
 
 
+mcp = FastMCP("AIMU Tools")
+
+@mcp.tool()
+def echo(echo_string: str) -> str:
+    """Returns echo_string."""
+    return echo_string
+
+if __name__ == "__main__":
+    mcp.run()
+
+
 class MCPClient:
     """A MCP client that wraps the FastMCP client for easier tool calls and management."""
 
-    def __init__(self, config: dict = None):
+    def __init__(self, config: dict = None, client: FastMCP = None, file: str = None):
+        self.client = None
+        if not config and not file:
+            self.client = client if client else mcp
+        
+        self.file = file
         self.config = config
+
         self.loop = asyncio.new_event_loop()
 
     async def _call_tool(self, tool_name: str, params: dict):
-        async with Client(self.config) as client:
+        if self.config:
+            async with Client(self.config) as client:
+                return await client.call_tool(tool_name, params)
+
+        if self.file:
+            async with Client(self.file) as client:
+                return await client.call_tool(tool_name, params)
+
+        async with Client(self.client) as client:
             return await client.call_tool(tool_name, params)
 
     def call_tool(self, tool_name: str, params: dict):
         return self.loop.run_until_complete(self._call_tool(tool_name, params))
 
     async def _list_tools(self):
-        async with Client(self.config) as client:
+        if self.config:
+            async with Client(self.config) as client:
+                return await client.list_tools()
+
+        if self.file:
+            async with Client(self.file) as client:
+                return await client.list_tools()
+
+        async with Client(self.client) as client:
             return await client.list_tools()
 
     def list_tools(self):
---
+++ tests/test_tools.py
@@ -0,0 +1,40 @@
+from aimu.tools import MCPClient
+
+def test_mcp_client_empty_init():
+    client = MCPClient()
+
+    assert client.client is not None
+    assert client.config is None
+
+    tools = client.list_tools()
+
+    assert isinstance(tools, list)
+    assert len(tools) > 0
+
+def test_mcp_client_with_config():
+    config =  {
+        "mcpServers": {
+            "echo": {"command": "python", "args": ["../aimu/tools.py"]}
+        },
+    }
+
+    client = MCPClient(config=config)
+
+    assert client.client is None
+    assert client.config == config
+
+    tools = client.list_tools()
+
+    assert isinstance(tools, list)
+    assert len(tools) > 0
+
+def test_mcp_client_with_file():
+    client = MCPClient(file="../aimu/tools.py")
+
+    assert client.client is None
+    assert client.config is None
+
+    tools = client.list_tools()
+
+    assert isinstance(tools, list)
+    assert len(tools) > 0
