a96bae478eeeb99458a0de61e0c9ac5816a81cc6
Paul R. Saxman <saxman@gmail.com>
2025-06-20 18:25:59 -0700

    feat: update test_models.py to include OllamaClient

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,15 +1,26 @@
 import pytest
 from typing import Iterable
 
-from aimu.models import ModelClient, HuggingFaceClient
+from aimu.models import ModelClient, HuggingFaceClient, OllamaClient
 
-MODELS = [HuggingFaceClient.MODEL_LLAMA_3_1_8B, HuggingFaceClient.MODEL_MISTRAL_7B]
+TEST_MODELS = [
+    HuggingFaceClient.MODEL_LLAMA_3_1_8B,
+    HuggingFaceClient.MODEL_MISTRAL_7B,
+    OllamaClient.MODEL_LLAMA_3_1_8B,
+    OllamaClient.MODEL_MISTRAL_7B,
+]
 
 
-@pytest.fixture(params=MODELS)
+@pytest.fixture(params=TEST_MODELS)
 def model_client(request) -> Iterable[ModelClient]:
-    client = HuggingFaceClient(request.param)
+    if ":" in request.param:
+        client = OllamaClient(request.param)
+    else:
+        client = HuggingFaceClient(request.param)
+
     yield client
+
+    del client
 
 
 def test_generate(model_client):
@@ -20,38 +31,11 @@ def test_generate(model_client):
     assert "Paris" in response
 
 
-def test_generate_with_parameters(model_client):
+def test_generate_streamed(model_client):
     prompt = "What is the capital of France?"
-    response = model_client.generate(prompt, generate_kwargs={"max_new_tokens": 1})
-
-    assert isinstance(response, str)
-    assert "Paris" in response
-
-
-def test_chat(model_client):
-    message = {"role": model_client.system_role, "content": "What is the capital of France?"}
-
-    response = model_client.chat(message)
-
-    assert "Paris" in response
-    assert len(model_client.messages) == 2
-
-    # The model should know that we're talking about capitals
-    message = {"role": "user", "content": "How about Germany's?"}
-
-    response = model_client.chat(message)
-
-    assert "Berlin" in response
-    assert len(model_client.messages) == 4
-
-
-def test_chat_streamed(model_client):
-    message = {"role": model_client.system_role, "content": "What is the capital of France?"}
-
-    response = model_client.chat_streamed(message)
+    response = model_client.generate_streamed(prompt)
 
     assert isinstance(response, Iterable)
-
     content = next(response)
     assert isinstance(content, str)
 
@@ -59,4 +43,45 @@ def test_chat_streamed(model_client):
         content += response_part
 
     assert "Paris" in content
+    assert isinstance(content, str)
+
+
+def test_generate_with_parameters(model_client):
+    prompt = "What is the capital of France?"
+    response = model_client.generate(prompt, generate_kwargs={"max_tokens": 10})
+
+    assert isinstance(response, str)
+    assert "Paris" in response
+
+
+def test_chat(model_client):
+    message = {"role": model_client.system_role, "content": "You are a helpful assistant."}
+    response = model_client.chat(message)
+
     assert len(model_client.messages) == 2
+
+    message = {"role": "user", "content": "What is the capital of France?"}
+    response = model_client.chat(message)
+
+    assert "Paris" in response
+    assert len(model_client.messages) == 4
+
+
+def test_chat_streamed(model_client):
+    message = {"role": model_client.system_role, "content": "You are a helpful assistant."}
+    response = model_client.chat(message)
+
+    assert len(model_client.messages) == 2
+
+    message = {"role": "user", "content": "What is the capital of France?"}
+    response = model_client.chat_streamed(message)
+
+    assert isinstance(response, Iterable)
+    content = next(response)
+    assert isinstance(content, str)
+
+    for response_part in response:
+        content += response_part
+
+    assert "Paris" in content
+    assert len(model_client.messages) == 4
