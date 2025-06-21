d2c87bd1b493a5ec1c30fb36aa19a5448a2bd7b3
Paul R Saxman <saxman@gmail.com>
2025-06-20 22:03:52 -0700

    feat: add paths.py to define project directory structure

---
+++ aimu/paths.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+root = Path(__file__).parents[1]
+
+# Path to the tests directory
+tests = root / "tests"
+
+# Path to the aimu package directory
+package = root / "aimu"
