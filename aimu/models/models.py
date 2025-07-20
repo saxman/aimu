08cc8c72eb746c599887bcdbe7ed2730c92d676f
Paul R Saxman <saxman@gmail.com>
2025-07-20 10:59:33 -0700

    Refactor ModelClient: replace model identifiers with enum and clean up unused model constants

--- aimu/models/models.py
+++ aimu/models/models.py
@@ -1,42 +1,19 @@
 import logging
+import enum
 
 logger = logging.getLogger(__name__)
 
+class Model(enum.Enum):
+    pass
 
 class ModelClient:
-    MODEL_LLAMA_3_1_8B = None
-    MODEL_LLAMA_3_2_3B = None
-    MODEL_LLAMA_3_3_70B = None
+    MODELS = Model
 
-    MODEL_GEMMA_3_12B = None
-
-    MODEL_PHI_4_14B = None
-    MODEL_PHI_4_MINI_3_8B = None
-
-    MODEL_DEEPSEEK_R1_8B = None
-
-    MODEL_MISTRAL_7B = None
-    MODEL_MISTRAL_NEMO_12B = None
-    MODEL_MISTRAL_SMALL_3_2_24B = None
-
-    MODEL_QWEN_3_8B = None
-
-    MODEL_SMOLLM2_1_7B = None
-    MODEL_SMOLLM3_3B = None
-
-    MODEL_GPT_4O_MINI = None
-    MODEL_GPT_4O = None
-
-    # TODO: add more models and enable use by clients
-    DEFAULT_MODEL_TEMPERATURES = {
-        MODEL_MISTRAL_NEMO_12B: 0.3,
-    }
-
-    def __init__(self, model_id: str, model_kwargs: dict = None, system_message: str = None):
-        if model_id is None:
+    def __init__(self, model: Model, model_kwargs: dict = None, system_message: str = None):
+        if model is None:
             raise ValueError("Model not supported by model client")
 
-        self.model_id = model_id
+        self.model = model
         self.model_kwargs = model_kwargs
         self.system_message = system_message
 
