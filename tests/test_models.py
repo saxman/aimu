139e8a9a992e6ae219453026557fc5fb6a4a7741
Paul R Saxman <saxman@gmail.com>
2025-09-28 12:00:26 -0700

    formatting

--- tests/test_models.py
+++ tests/test_models.py
@@ -145,7 +145,7 @@ def test_chat_streamed(model_client):
     response = model_client.chat_streamed("What is the capital of France?")
 
     assert isinstance(response, Iterable)
-    content = next(response) # type: ignore
+    content = next(response)  # type: ignore
     assert isinstance(content, str)
 
     for response_part in response:
